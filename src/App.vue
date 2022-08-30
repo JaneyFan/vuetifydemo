6cef1d53429d8f63043435d2b373ad39c1d4609b
JaneyFan <jian0177@easv365.dk>
2022-08-30 08:30:38 +0200

    Add all my files

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,49 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app>
+    <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        expand-on-hover
+        rail
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            title="Sandra Adams"
+            subtitle="sandra_a88@gmailcom"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <router-link to="/">
+            <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+          </router-link>
+          <router-link to="/about">
+            <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+          </router-link>
+            <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+          <Suspense>
+            <router-view></router-view>
+          </Suspense>
+        </v-main>
+    </v-layout>
+  </v-card>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>
+      Front page stuff
+    </h1>
+    <v-card class="ma-5 pa-4" v-for="post in posts" :key="post">
+      <p>Title: {{post.title}}</p>
+      <p>Description: {{post.description}}</p>
+      <p>QuoteNumber: {{post.quoteNumber}}</p>
+      <p>PostID: {{post.id}}</p>
+      <v-btn @click="firebaseDeleteSingleItem(post.id)">delete</v-btn>
+    </v-card>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup>
+import { db } from '../firebase.js'
+import { ref } from 'vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
+
+const posts = ref([]) 
+
+const firebaseDeleteSingleItem = async(id) => {
+  await deleteDoc(doc(db, "posts", id));
+  console.log("Data deleted", id)
+} 
+
+const querySnapshot = await getDocs(collection(db, "posts"));
+querySnapshot.forEach((doc) => {
+  // doc.data() is never undefined for query doc snapshots
+    posts.value.push({ 
+    ...doc.data(), 
+    id: doc.id 
+  })
+
+
+  console.log(doc.id, " => ", doc.data());
+});
 </script>
